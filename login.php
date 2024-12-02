8035e0779ab905fdef96b6525a1de8cda4415f89
WarrenEllsworth <48393777+WarrenEllsworth@users.noreply.github.com>
2024-12-02 10:28:56 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,1 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    // Dummy check for username and password (replace with your own authentication logic)
-    if ($username === "admin" && $password === "password123") {
-        header("Location: admin-dashboard.html");
-        exit;
-    } else {
-        echo "Invalid username or password.";
-    }
-}
-?>
